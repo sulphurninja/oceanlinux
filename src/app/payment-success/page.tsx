1e1c6ad5f331ef33f41758291bfe9bfd2b8a3d49
sulphurninja <sulphurninja@gmail.com>
2025-02-02 18:49:16 +0530

    Update page.tsx

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -9,35 +9,35 @@ const PaymentSuccessPage = () => {
     const searchParams = useSearchParams();
     const orderId = searchParams.get('order_id');
 
-    useEffect(() => {
-        if (orderId) {
-            verifyPayment(orderId);
-        }
-    }, [orderId]);
+    // useEffect(() => {
+    //     if (orderId) {
+    //         verifyPayment(orderId);
+    //     }
+    // }, [orderId]);
 
-    const verifyPayment = async (orderId) => {
-        try {
-            const response = await fetch('/api/confirmOrder', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ orderId })
-            });
+    // const verifyPayment = async (orderId) => {
+    //     try {
+    //         const response = await fetch('/api/confirmOrder', {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({ orderId })
+    //         });
 
-            const confirmData = await response.json();
+    //         const confirmData = await response.json();
 
-            if (confirmData.message === "Order confirmed.") {
-                toast.success("Payment Verified! Order Created Successfully.");
-                router.push('/dashboard/viewLinux');
-            } else {
-                toast.error("Payment Verification Failed.");
-                router.push('/dashboard');
-            }
-        } catch (error) {
-            console.error("Error verifying payment:", error);
-            toast.error("Something went wrong.");
-            router.push('/dashboard');
-        }
-    };
+    //         if (confirmData.message === "Order confirmed.") {
+    //             toast.success("Payment Verified! Order Created Successfully.");
+    //             router.push('/dashboard/viewLinux');
+    //         } else {
+    //             toast.error("Payment Verification Failed.");
+    //             router.push('/dashboard');
+    //         }
+    //     } catch (error) {
+    //         console.error("Error verifying payment:", error);
+    //         toast.error("Something went wrong.");
+    //         router.push('/dashboard');
+    //     }
+    // };
 
     return (
         <div className="flex flex-col items-center bg-background justify-center min-h-screen">
